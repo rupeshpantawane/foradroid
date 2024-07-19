bfbd13783bd20a8d48599bfa28e380a2f4f3d395
Rupesh Pantawane <rupeshpantawane62@gmail.com>
2024-07-19 17:25:43 +0530

    .

--- rupesh/routes/base.router.js
+++ rupesh/routes/base.router.js
@@ -10,29 +10,6 @@ require("../middleware/passport")(passport);
 const adminMidd = require('../middleware/admin.middleware');
 
 
-
-const SimpleCrudController = require("../controllers/admin/simplecrud.controller");
-
-
-
-
-
-
-
-
-// Start SimpleCrudController
-baseRouter.post("/fetch-simple-cruds", passport.authenticate("jwt", { session: false }), adminMidd.checkUser, SimpleCrudController.fetchSimpleCrudsWithPeginate);
-baseRouter.post("/create-simple-crud", passport.authenticate("jwt", { session: false }), adminMidd.checkUser, SimpleCrudController.createSimpleCrud);
-baseRouter.get("/fetch-simple-crud/:simple_crud_id", passport.authenticate("jwt", { session: false }), adminMidd.checkUser, SimpleCrudController.fetchSimpleCrud);
-baseRouter.post("/update-simple-crud", passport.authenticate("jwt", { session: false }), adminMidd.checkUser, SimpleCrudController.updateSimpleCrud);
-baseRouter.delete("/delete-simple-crud/:simple_crud_id", passport.authenticate("jwt", { session: false }), adminMidd.checkUser, SimpleCrudController.deleteSimpleCrud);
-baseRouter.post("/user/register",  SimpleCrudController.register);
-baseRouter.post("/payment/method/attach",  SimpleCrudController.attach);
-baseRouter.get("/payment/methods",  SimpleCrudController.methods);
-baseRouter.post("/payment/create",  SimpleCrudController.create);
-baseRouter.post("/payment/confirm",  SimpleCrudController.confirm);
-baseRouter.post("/payment/confirm",  SimpleCrudController.confirm);
-
 // End SimpleCrudController
 
 
